917164b0b5c14ac41b22507678e438cdbb1066d0
Eber <Eber@EBER-NOTE>
2016-08-23 15:09:48 -0300

    Add project files.

---
+++ App2/App2/MyPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModel();
+        }
+    }
+}
---
+++ App2/App2/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App2
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string texto;
+
+        public string Texto
+        {
+            get
+            {
+                return texto;
+            }
+
+            set
+            {
+                texto = value;this.Notify("Texto");
+            }
+        }
+
+        public ICommand ToqueCommand
+        {
+            get;
+            set;
+        }
+
+        public ViewModel()
+        {
+            this.ToqueCommand = new Command(this.Toque);
+        }
+
+        private int i = 1;
+        private void Toque(object parameter)
+        {
+            this.Texto = string.Format("Toque número {0}", i++);
+            Device.OpenUri(new Uri("http://www.fatecjd.edu.br/site/"));
+        }
+
+        private void Notify(string parameter)
+        {
+            if(this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(parameter));
+            }
+        }
+    }
+}
